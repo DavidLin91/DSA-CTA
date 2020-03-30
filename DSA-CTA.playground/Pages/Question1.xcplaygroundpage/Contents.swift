89e42556f3eb1015f6f3b2599a69a6f4b0f8176a
David <lin.david985@gmail.com>
2020-03-30 10:50:43 -0400

    added question 1

---
+++ DSA-CTA.playground/Pages/Question1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//Question 1
+
+class Node<T> {
+    var next: Node?
+    let val: T
+    init(val: T) {
+        self.val = val
+    }
+}
+
+// Print format:
+
+//```
+//-> 4 -> 9 -> 2 -> 3 -> nil
+//```
